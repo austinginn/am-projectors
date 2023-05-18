ae31ae2820a7d203232064f7810b1fdeca5ce487
Austin Ginn <austinleeginn@gmail.com>
2023-05-18 16:53:51 -0400

    ready for testing power on and general functions

--- src/main.js
+++ src/main.js
@@ -1,36 +1,80 @@
-const { Telnet } = require('telnet-client');
+import { Telnet } from 'telnet-client';
+import config from '../data/config.json';
+import models from '../data/models.json';
 
-//load json
+//check for all args present
+if (process.argv.length !== 4) {
+  console.error('Expected 2 arguments - {NAME} {CMD}');
+  process.exit(1);
+}
 
-//command on endpoint
+//run cmd
+if (process.argv[3] === "on") {
+  await powerOn(process.argv[2]);
+}
 
-//command off endpoint
+if(process.argv[3] === "off") {
+  await powerOff(process.argv[2]);
+}
 
-//status endpoint
+if(process.argv[3] === "status") {
+  await powerOff(process.argv[2]);
+}
 
-(async function () {
-  const connection = new Telnet()
+console.log("done");
 
-  // these parameters are just examples and most probably won't work for your use-case.
+//connect via telnet
+async function telnetConnect(ip, port, shell, timeout) {
+  const connection = new Telnet();
+
   const params = {
-    host: '127.0.0.1',
-    port: 23,
-    shellPrompt: '/ # ', // or negotiationMandatory: false
-    timeout: 1500
+    host: host,
+    port: port,
+    shellPrompt: shell, // or negotiationMandatory: false
+    timeout: timeout
   }
 
   try {
     await connection.connect(params)
   } catch (error) {
-    // handle the throw (timeout)
+    console.error(error.msg);
   }
 
-  const res = await connection.exec('uptime')
-  console.log('async result:', res)
-})()
-Callback-style
-const { Telnet } = require('telnet-client')
-const connection = new Telnet()
+  return connection;
+}
 
-// these parameters are just examples and most probably won't work for your use-case.
-const
+async function powerOn(name) {
+  //find index to item in config based on name
+  const index = config.devices.findIndex(d => d.name == name);
+
+  //check if name was found
+  if(index == -1){
+    console.error("device with that name was not found");
+    return -1;
+  }
+
+  const device = config.devices[index];
+
+  //find index in models to specific model
+  const model_index = models.models.findIndex(m => m.model == device.model);
+
+  //check if model was found
+  if(model_index == -1){
+    console.error("model was not found")
+    return -1;
+  }
+
+  const model = models.models[model_index];
+
+  //connect
+  const connection = await telnetConnect(device.ip, model.port, model.shell, model.timeout);
+
+  //power on
+  const res = await connection.exec(model.on);
+
+  console.log(res);
+
+  connection.end();
+
+  return res;
+}
